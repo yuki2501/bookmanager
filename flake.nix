b8c628d5b65f6528a22fb8428c2aa35b666b8328
yuki25011 <yuki2501@pm.me>
2024-11-21 11:03:55 +0900

    add some setting

--- flake.nix
+++ flake.nix
@@ -1,30 +1,38 @@
 {
+  description = "bookmanager";
   inputs = {
-    typelevel-nix.url = "github:typelevel/typelevel-nix";
-    nixpkgs.follows = "typelevel-nix/nixpkgs";
-    flake-utils.follows = "typelevel-nix/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=24.05";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
       let
+        config = {
+          allowUnfree = true;
+        };
+        overlays = [
+          (final: prev: {
+            sbt = prev.sbt.overrideAttrs (oldAttrs: {
+              postPatch = "";
+            });
+          })
+        ];
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ typelevel-nix.overlays.default ];
+          overlays = overlays;
+          config = config;
         };
       in
       {
-        devShell = pkgs.devshell.mkShell {
-          imports = [ typelevel-nix.typelevelShell ];
-          name = "my-project-shell";
-          typelevelShell = {
-            jdk.package = pkgs.jdk8;
-            sbtMicrosites = {
-              enable = true;
-              siteDir = ./site;
-            };
-          };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            coursier
+            scala_3
+            scala-cli
+            scalafmt
+            ammonite
+          ];
         };
-      }
-    );
+      });
 }
